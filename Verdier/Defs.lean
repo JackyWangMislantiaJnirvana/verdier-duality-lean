3d8cc9717e14a628c9526b4974eb09f9f99d697d
Jack Wang <wmjwld@gmail.com>
2025-06-09 01:39:58 +0000

    skeleton of main theorem, but a mess

--- Verdier/Defs.lean
+++ Verdier/Defs.lean
@@ -8,8 +8,8 @@ universe v v' u w
 Base space:
 locally compact space of finite cohomological dim
 -/
-variable (X : TopCat.{u})
-variable (Y : TopCat.{u})
+variable (X : TopCat.{u}) [LocallyCompactSpace X] [T2Space X]
+variable (Y : TopCat.{u}) [LocallyCompactSpace Y] [T2Space Y]
 
 /-!
 Base ring:
@@ -36,6 +36,7 @@ C⁺(X)
 
 instance (base : TopCat) : Preadditive (Sh R base) := instPreadditiveSheaf
 instance (base : TopCat) : Abelian (Sh R base) := sorry
+instance (base : TopCat) : HasDerivedCategory (Sh R base) := sorry
 
 abbrev C (base : TopCat.{u}) := CochainComplex (Sh.{v} R base) ℤ
 
@@ -61,34 +62,89 @@ This sums up to the "m aking derivation" map
 R(-) : (Sh(X) ⥤ Sh(Y)) → (D⁺(X) ⥤ D⁺(Y))
 -/
 
-def direct_image_proper_support (f : X → Y) (p_cont : Continuous f) : Sh.{v, u} R X ⥤ Sh.{v, u} R Y := sorry
+def direct_image_proper_support (f : X → Y) (p_cont : Continuous f) :
+  Sh.{v, u} R X ⥤ Sh.{v, u} R Y where
+    obj ℱ := {
+      val := {
+        obj := sorry
+        map := sorry
+      }
+      cond := sorry
+    }
+    map g := sorry
 
--- We need a function that makes f_! a functor on chain complexes.
--- : whaaat type??? Think it through
-def functor_to_chain_map (F : Sh R X ⥤ Sh R Y) : C.{v, u} R X ⥤ C.{v, u} R Y := sorry
+def direct_image (f : X → Y) (p_cont : Continuous f) :
+  Sh.{v, u} R X ⥤ Sh.{v, u} R Y where
+    obj ℱ := {
+      val := {
+        obj := sorry
+        map := sorry
+      }
+      cond := sorry
+    }
+    map g := sorry
+
+instance (f : X → Y) (p_cont : Continuous f) :
+  -- Functor.Additive (direct_image_proper_support.{v, u} X Y R f p_cont) := sorry
+  Functor.Additive (direct_image_proper_support.{v, u} X Y R f p_cont) := sorry
+
+def functor_to_chain_map (F : Sh R X ⥤ Sh R Y) :
+  C.{v, u} R X ⥤ C.{v, u} R Y where
+    obj A := sorry
+    map g := sorry
 
 def derived (F : C R X ⥤ C R Y) : D.{v', u} R X ⥤ D.{v', u} R Y := sorry
 
 #check derived X Y R (functor_to_chain_map X Y R sorry)
 
-example : derived X Y R sorry = sorry := sorry
-example : derived X Y R (functor_to_chain_map X Y R sorry) = sorry := sorry
-example : derived X Y R (functor_to_chain_map X Y R (direct_image_proper_support X Y R sorry sorry)) = sorry := sorry
-
--- why does this work???
+-- why does this universe assignment work???
 -- how is the order of XXX.{u, v} matched to the variables in the function definition?
-abbrev R! (f : X -> Y) (p_cont : Continuous f) := derived.{v', u} X Y R (functor_to_chain_map.{v, u} X Y R (direct_image_proper_support.{v, u} X Y R f p_cont))
+set_option pp.universes true
+abbrev R! (f : X → Y) (p_cont : Continuous f): D.{v', u} R X ⥤ D.{v', u} R Y :=
+  derived.{v', u} X Y R (
+    functor_to_chain_map.{v, u} X Y R (
+      direct_image_proper_support.{v, u} X Y R f p_cont))
+
+abbrev Rstar (f : X → Y) (p_cont : Continuous f): D.{v', u} R X ⥤ D.{v', u} R Y :=
+  derived.{v', u} X Y R (
+    functor_to_chain_map.{v, u} X Y R (
+      direct_image.{v, u} X Y R f p_cont))
+
+-- Maybe a better way to define R!
+-- noncomputable def CategoryTheory.Functor.mapDerivedCategory
+--   {C₁ : Type u₁} [Category.{v₁, u₁} C₁] [Abelian C₁] [HasDerivedCategory C₁]
+--   {C₂ : Type u₂} [Category.{v₂, u₂} C₂] [Abelian C₂] [HasDerivedCategory C₂]
+--   (F : Functor C₁ C₂) [F.Additive]
+--   [Limits.PreservesFiniteLimits F] [Limits.PreservesFiniteColimits F] :
+--   Functor (DerivedCategory C₁) (DerivedCategory C₂)
+-- set_option trace.Meta.synthInstance true
+abbrev R!' (f : X → Y) (p_cont : Continuous f) :=
+  -- Functor.mapDerivedCategory (direct_image_proper_support.{v, u} X Y R f p_cont)
+  Functor.mapDerivedCategory (direct_image_proper_support.{v, u} X Y R f p_cont)
+
 
 /-!
 Define/search for HomSheafComplex
 and then define its right derived functor
 -/
 
+def sheaf_hom (F G : Sh R X) : Sh R X := sorry
+
+def sheaf_hom_complex (F G : C R X) : C R X := sorry
+
+def derived_sheaf_hom_complex (F G : D R X) : D R X := sorry
+
 /-
 Statement of the main theorem
 -/
-
-
+#check D.{v, v', u, w} R X
+variable (f : X → Y) (p_cont : Continuous f) (F : D R X) (G : D R Y)
+theorem local_verdier_duality :
+  ∃ g : D R Y ⥤ D R X,
+  Iso
+    (derived_sheaf_hom_complex Y R ((R! X Y R f p_cont).obj F) G)
+    ((Rstar X Y R f p_cont).obj (derived_sheaf_hom_complex X R F (g.obj G)))
+  := sorry
 
 section SheafExperiment
 -- Abelian sheaf
